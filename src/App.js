2d1462c37168ed5e4696d27edc2813a0d74c39dd
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-15 13:01:44 +0600

    Home component created and routec.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
@@ -23,6 +25,11 @@ function App() {
       */}
 
       <Header></Header>
+
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
 
 
     </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,9 @@ const Header = () => {
     return (
         <div>
             Header.
+            <nav>
+
+            </nav>
         </div>
     );
 };
