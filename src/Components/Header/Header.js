282186bfdb121853b7034696d68cd00219ee6e19
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-15 15:36:44 +0600

    all header icon added and linked.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HomeIcon, IdentificationIcon, DocumentIcon, SparklesIcon, AcademicCapIcon, UserGroupIcon, NewspaperIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
     return (
@@ -8,11 +9,26 @@ const Header = () => {
                 Logo
             </div>
             <div>
-                Header
+                Header Title
             </div>
             <div>
                 <nav>
-                    nav
+                    <Link to="/"><HomeIcon className='h-6 w-6 text-white'></HomeIcon></Link>
+
+                    <Link to="/about"><IdentificationIcon className='h-6 w-6 text-white'></IdentificationIcon></Link>
+
+                    <Link to="/resume"><DocumentIcon className='h-6 w-6 text-white'></DocumentIcon></Link>
+
+                    <Link to="/services"><SparklesIcon className='h-6 w-6 text-white'></SparklesIcon></Link>
+
+                    <Link to="/skills"><AcademicCapIcon className='h-6 w-6 text-white'></AcademicCapIcon></Link>
+
+                    <Link to="/portfolios"><UserGroupIcon className='h-6 w-6 text-white'></UserGroupIcon></Link>
+
+                    <Link to="/testimonial"><NewspaperIcon className='h-6 w-6 text-white'></NewspaperIcon></Link>
+
+                    <Link to="/contact"><EnvelopeIcon className='h-6 w-6 text-white'></EnvelopeIcon></Link>
+
                 </nav>
             </div>
         </div>
